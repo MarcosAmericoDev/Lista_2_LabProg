6816841b34b17009b41ae3d32561e53b09bdf661
Marcos Iury de Lima Américo <MarcosIuryProgramador@gmail.com>
2023-02-23 18:02:43 -0300

    Adicionando a questao 4

---
+++ 04.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    int AlturaJoaoCentimetros, AlturaPedroCentimetros, Anos;
+
+    AlturaJoaoCentimetros = 150;
+    AlturaPedroCentimetros = 110;
+    Anos = 0;
+
+    for(;;)
+    {
+        if (AlturaJoaoCentimetros == AlturaPedroCentimetros)
+        {
+            break;
+        }
+
+        AlturaJoaoCentimetros += 2;
+        AlturaPedroCentimetros += 3;
+
+        Anos++;
+    }
+    
+    printf("Pedro vai demorar %d anos para alcançar a altura de João, ambos com %d", Anos, AlturaJoaoCentimetros);
+    return 0;
+}
